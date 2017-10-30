9ea9900a2813842e65a40d2239b1e6fea26858e9
drstonebraker <drew.stonebraker@gmail.com>
2017-10-29 23:19:51 -0700

    Complete lessons

--- 1-12-skeleton/service-worker.js
+++ 1-12-skeleton/service-worker.js
@@ -1,9 +1,10 @@
-
 var cacheName = 'weatherPWA-v2';
+var dataCacheName = 'weatherData-v2';
 var filesToCache = [
   '/',
   '/index.html',
   '/scripts/app.js',
+  '/scripts/localforage-1.4.0.js',
   '/styles/ud811.css',
   '/images/clear.png',
   '/images/cloudy-scattered-showers.png',
@@ -17,38 +18,52 @@ var filesToCache = [
   '/images/sleet.png',
   '/images/snow.png',
   '/images/thunderstorm.png',
-  '/images/wind.png'
+  '/images/win d.png'
 ];
+var weatherAPIUrlBase = 'https://api.darksky.net/forecast/0596156d5c90ef6dd7119a8491ae874d/42.3601,-71.0589';
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache)
+      return cache.addAll(filesToCache);
     })
-  )
-})
+  );
+});
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
+        if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key)
+          return caches.delete(key);
         }
-      }))
+      }));
     })
-  )
-})
+  );
+});
 
 self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request)
-    })
-  )
-})
+  if (e.request.url.startsWith(weatherAPIUrlBase)) {
+    e.respondWith(
+      fetch(e.request)
+        .then(function(response) {
+          return caches.open(dataCacheName).then(function(cache) {
+            cache.put(e.request.url, response.clone());
+            console.log('[ServiceWorker] Fetched & Cached', e.request.url);
+            return response;
+          });
+        })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        console.log('[ServiceWorker] Fetch Only', e.request.url);
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
